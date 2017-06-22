bcac16fdd0cc850e36aa007f5f200b737cdcad72
Rakesh Jha <29140@corp.hexaware.com>
2017-06-22 17:54:30 +0530

    inital checking

---
+++ Citiassitant.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var bodyParser = require('body-parser');
+var apiai=require('apiai');
+var aiengine=apiai('4d2ce4a4e2834ce084357c56bf2afa7b');
+var app=express();
+var port=process.env.PORT||5001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post ('/',function(request,response){
+console.log(JSON.stringify(request.body));
+var apiagentreq= request.body.result && request.body.result.parameters;
+var resagent='How can i help You ';
+console.log('request are'+apiagentreq);
+return response.json({
+  speech:resagent,
+  displayText: resagent,
+  source:''
+});
+});
+
+
+app.listen(port,function(){
+console.log("App is running in port" +port);
+});
