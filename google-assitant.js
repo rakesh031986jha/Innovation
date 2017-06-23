a06fb01c16afddef780f092fbc7538ad2f63ce79
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-23 16:21:56 +0530

    Update google-assitant.js

--- google-assitant.js
+++ google-assitant.js
@@ -7,13 +7,14 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.post('/', function (req, res) {
-  console.log(JSON.stringify(req.body));
-  const assistant = new Assistant({request: req, response: res});
 
-  function simpleResponse (Assistant) {
-    //const app = new Assistant({request: req,response: res});
+  console.log(JSON.stringify(req.body));
+// for google assistant request
+  if(req.body.originalRequest.source===google){
+  const assistant = new Assistant({request: req, response: res});
+function simpleResponse (Assistant) {
+
     console.log("Preparing response ")
     assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
               'almost any number, like 42. What do you have in mind?',
@@ -21,8 +22,21 @@ app.post('/', function (req, res) {
                     'number. What do you have in mind?'})
   };
 assistant.handleRequest(simpleResponse);
-});
+}
 
+
+// For slack request
+  if(req.body.originalRequest.source===slack){
+var apiagentreq= req.body.result && req.body.result.parameters;
+var resagent='How can i help You ';
+console.log('request are'+apiagentreq);
+return response.json({
+  speech:resagent,
+  displayText: resagent,
+  source:''
+});
+}
+});
 
 //Assistant.handleRequest(simpleResponse);
 var port = process.env.PORT || 4000;
