49f25ae6385dfe535b42c36b8f6555d67f54f06c
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-23 17:11:39 +0530

    Update google-assitant.js

--- google-assitant.js
+++ google-assitant.js
@@ -7,14 +7,13 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.post('/', function (req, res) {
-
   console.log(JSON.stringify(req.body));
-// for google assistant request
-  if(req.body.originalRequest.source===google){
   const assistant = new Assistant({request: req, response: res});
-function simpleResponse (Assistant) {
 
+  function simpleResponse (Assistant) {
+    //const app = new Assistant({request: req,response: res});
     console.log("Preparing response ")
     assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
               'almost any number, like 42. What do you have in mind?',
@@ -22,21 +21,8 @@ function simpleResponse (Assistant) {
                     'number. What do you have in mind?'})
   };
 assistant.handleRequest(simpleResponse);
-}
-
-
-// For slack request
-  if(req.body.originalRequest.source===slack){
-var apiagentreq= req.body.result && req.body.result.parameters;
-var resagent='How can i help You ';
-console.log('request are'+apiagentreq);
-return response.json({
-  speech:resagent,
-  displayText: resagent,
-  source:''
 });
-}
-});
+
 
 //Assistant.handleRequest(simpleResponse);
 var port = process.env.PORT || 4000;
