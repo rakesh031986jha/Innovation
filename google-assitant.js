d45ee42564282f4db791cf8b2709a36c14307ff2
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-23 13:03:16 +0530

    Update google-assitant.js

--- google-assitant.js
+++ google-assitant.js
@@ -15,8 +15,10 @@ app.post('/', function (req, res) {
   function simpleResponse (Assistant) {
     //const app = new Assistant({request: req,response: res});
     console.log("Preparing response ")
-  app.ask({speech: 'Howdy! I can tell you fun facts about '
-          })
+    app.ask({speech: 'Howdy! I can tell you fun facts about ' +
+              'almost any number, like 42. What do you have in mind?',
+              displayText: 'Howdy! I can tell you fun facts about almost any ' +
+                    'number. What do you have in mind?'})
   };
 assistant.handleRequest(simpleResponse);
 });
