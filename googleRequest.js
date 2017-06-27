299676deb98d8078928aee9157508e64b6dac2ea
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 12:54:03 +0530

    Update googleRequest.js

--- googleRequest.js
+++ googleRequest.js
@@ -6,14 +6,35 @@ function simpleResponse (req, res) {
   const assistant = new Assistant({request: req, response: res});
 
     console.log("Preparing response ");
-    assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
-              'almost any number, like 42. What do you have in mind?',
-              displayText: 'Howdy! I can tell you fun facts about almost any ' +
-                    'number. What do you have in mind?'})
+//    assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
+  //            'almost any number, like 42. What do you have in mind?',
+    //          displayText: 'Howdy! I can tell you fun facts about almost any ' +
+      //              'number. What do you have in mind?'})
 
-assistant.handleRequest(simpleResponse);
+  function welcomeIntent (assistant) {
+    assistant.ask('Welcome to cityassistant..how can i help you...',
+    ['Say any number', 'Pick a number', 'We can stop here. See you soon.']);
+                    }
+
+
+function rawInput (assistant) {
+      if (assistant.getRawInput() === 'bye') {
+       assistant.tell('Goodbye!');
+      } else {
+       const inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
+          assistant.getRawInput() + '</say-as></speak>',
+            ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+        assistant.ask(inputPrompt);
+      }
+    }
+    const actionMap = new Map();
+    actionMap.set(WELCOME_INTENT, welcomeIntent);
+    actionMap.set(LOOKUP_INTENT, lookupdataIntent);
+    assistant.handleRequest(actionMap);
+
 
 }
+
 var exports = module.exports = {
   simpleResponse: simpleResponse
 };
