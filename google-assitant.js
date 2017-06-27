1dac9664a317a6dfb6ea0e13355e39471d21f38b
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 11:51:13 +0530

    Update google-assitant.js

--- google-assitant.js
+++ google-assitant.js
@@ -13,7 +13,7 @@ app.post('/', function (req, res) {
   console.log(JSON.stringify(req.body));
 // for google assistant request
   if(req.body.originalRequest.source==='google'){
-      console.log("comeing in side ");
+    console.log("comeing in side ");
   googleRequest.simpleResponse(req, res);
 }
    // For slack request
