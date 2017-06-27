d928cf3fe2f5f48f7cf5369e5920dc63b002d741
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 09:47:52 +0530

    Create googleRequest.js
    initial file

---
+++ googleRequest.js
@@ -0,0 +1,16 @@
+
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+
+function simpleResponse (req) {
+  const assistant = new Assistant({request: req, response: res});
+
+    console.log("Preparing response ");
+    assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
+              'almost any number, like 42. What do you have in mind?',
+              displayText: 'Howdy! I can tell you fun facts about almost any ' +
+                    'number. What do you have in mind?'})
+
+assistant.handleRequest(simpleResponse);
+
+}
