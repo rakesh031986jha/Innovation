17ae1acf95038562291c64abf1e0b33ae8ac94b5
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 13:06:51 +0530

    Update googleRequest.js

--- googleRequest.js
+++ googleRequest.js
@@ -6,11 +6,6 @@ function simpleResponse (req, res) {
   const assistant = new Assistant({request: req, response: res});
 
     console.log("Preparing response ");
-//    assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
-  //            'almost any number, like 42. What do you have in mind?',
-    //          displayText: 'Howdy! I can tell you fun facts about almost any ' +
-      //              'number. What do you have in mind?'})
-
       function mainIntent (assistant) {
           const inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
           'I can read out an ordinal like ' +
@@ -18,10 +13,10 @@ function simpleResponse (req, res) {
           ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
             assistant.ask(inputPrompt);
          }
-                    
+
 
     const actionMap = new Map();
-    actionMap.set(assistant.WELCOME_INTENT, welcomeIntent);
+    actionMap.set(WELCOME_INTENT, welcomeIntent);
     assistant.handleRequest(actionMap);
 
 
