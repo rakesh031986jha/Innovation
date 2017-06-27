7aaf94e74e9deb982dc31c3d093adb6da6181525
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 13:17:10 +0530

    Update googleRequest.js

--- googleRequest.js
+++ googleRequest.js
@@ -9,13 +9,7 @@ function simpleResponse (req, res) {
       function mainIntent (assistant) {
         if (assistant.getRawInput() === 'Hi') {
        assistant.tell('How can i help you!');
-      }else{
-          const inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
-          'I can read out an ordinal like ' +
-          '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
-          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-            assistant.ask(inputPrompt);
-         }
+      }
 }
 
     assistant.handleRequest(assistant);
