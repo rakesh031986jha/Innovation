46c98fe230012e9bf4302604928021b4cd166267
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 09:48:51 +0530

    Create slackrequest.js
    new file for

---
+++ slackrequest.js
@@ -0,0 +1,12 @@
+
+
+function  slackReq(){
+var apiagentreq= req.body.result && req.body.result.parameters;
+var resagent='How can i help You ';
+console.log('request are'+apiagentreq);
+return response.json({
+  speech:resagent,
+  displayText: resagent,
+  source:''
+});
+}
