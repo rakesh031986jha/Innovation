8e51ffa0c499b6c965c044642eebeb0fcef95a7b
rakesh031986jha <rakesh031986jha@gmail.com>
2017-06-27 09:46:29 +0530

    Update google-assitant.js

--- google-assitant.js
+++ google-assitant.js
@@ -1,30 +1,31 @@
 
-let Assistant = require('actions-on-google').ApiAiAssistant;
+//let Assistant = require('actions-on-google').ApiAiAssistant;
 let request = require('http');
 let express = require('express');
 let bodyParser  = require('body-parser')
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+var googleRequest = require('./googleRequest');
+var slackrequest = require('./slackrequest');
 
 app.post('/', function (req, res) {
   console.log(JSON.stringify(req.body));
-  const assistant = new Assistant({request: req, response: res});
-
-  function simpleResponse (Assistant) {
-    //const app = new Assistant({request: req,response: res});
-    console.log("Preparing response ")
-    assistant.ask({speech: 'Howdy! I can tell you fun facts about ' +
-              'almost any number, like 42. What do you have in mind?',
-              displayText: 'Howdy! I can tell you fun facts about almost any ' +
-                    'number. What do you have in mind?'})
-  };
-assistant.handleRequest(simpleResponse);
+// for google assistant request
+  if(req.body.originalRequest.source==='google'){
+  googleRequest.simpleResponse(req);
+}
+   // For slack request
+  if(req.body.originalRequest.source==='slack'){
+    slackrequest.slackReq();
+}
 });
-
 
 //Assistant.handleRequest(simpleResponse);
 var port = process.env.PORT || 4000;
 app.listen(port);
     console.log('App listening on port %s', port);
+
+//module =googleRequest;
+module.exports.googleRequest = googleRequest;
+module.exports.slackrequest = slackrequest;
